398d10a5ff2fd21fd892c75bfd68ff4fc94c10e4
guoqianling <guoqianling@babytree-inc.com>
2017-03-14 10:48:29 +0800

    INIT COMMIT

---
+++ MGVideo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MGVideo
+//
+//  Created by guoqianling on 2017/3/14.
+//  Copyright © 2017年 MarvesG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ MGVideo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MGVideo
+//
+//  Created by guoqianling on 2017/3/14.
+//  Copyright © 2017年 MarvesG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
